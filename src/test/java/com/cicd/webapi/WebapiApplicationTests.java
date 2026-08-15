36c7c94f6601d605308a83cb0ac17798794aa3dd
PabloV <p.villazon@gmail.com>
2026-08-14 21:14:01 -0400

    Fix includes

--- src/test/java/com/cicd/webapi/WebapiApplicationTests.java
+++ src/test/java/com/cicd/webapi/WebapiApplicationTests.java
@@ -4,7 +4,7 @@ package com.cicd.webapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WebapiApplicationTests {
 
 	@Autowired
