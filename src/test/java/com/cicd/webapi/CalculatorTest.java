1ba9173881e185f2b29877bfb2c2e47ae9f2b3d6
PabloV <p.villazon@gmail.com>
2026-08-17 19:50:20 -0400

    Agregar clases nuevas y sus pruebas unitarias

---
+++ src/test/java/com/cicd/webapi/CalculatorTest.java
@@ -0,0 +1,45 @@
+package com.cicd.webapi;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class CalculatorTest {
+    
+    @Test
+    void testAdd() {
+        Calculator calculator = new Calculator();
+        assert calculator.add(2, 3) == 5;
+        assert calculator.add(-2, -3) == -5;
+    }
+
+    @Test
+    void testSubtract() {
+        Calculator calculator = new Calculator();
+        assert calculator.subtract(5, 3) == 2;
+
+        assert calculator.subtract(-5, 3) == -8;
+    }
+
+    @Test
+    void testMultiply() {
+        Calculator calculator = new Calculator();
+        assert calculator.multiply(2, 3) == 6;
+        assert calculator.multiply(0, 3) == 0;
+        assert calculator.multiply(1, -3) == -3;
+    }
+
+    @Test
+    void testDivide() {
+        Calculator calculator = new Calculator();
+        assert calculator.divide(10, 2) == 5;
+
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class,
+                () -> calculator.divide(10, 0)
+            );
+
+        assert exception.getMessage().equals("Denominator cannot be zero");
+        
+    }
+}
