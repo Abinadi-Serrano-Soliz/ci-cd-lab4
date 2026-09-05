61bc8bb250d08c332657263fcb2d19c08ccc4c4b
Abinadi-Serrano-Soliz <abinadiserranosoliz@gmail.com>
2026-09-04 21:15:16 -0400

    pruebas de proteccion solucionado

--- src/test/java/com/cicd/webapi/WebapiApplicationTests.java
+++ src/test/java/com/cicd/webapi/WebapiApplicationTests.java
@@ -46,7 +46,7 @@ class WebapiApplicationTests {
 		MockMvc.perform(get("/")
 				.accept(MediaType.TEXT_PLAIN))
 			.andExpect(status().isOk())
-			.andExpect(content().string(/*"GRUPO 12:\n\n1. Camargo Soleto Gustavo Sebastián\n2. Saavedra Gómez Henry\n3. Villca Quiroga Ronald Nelson\n4. Serrano Soliz Abinadi"*/));
+			.andExpect(content().string("GRUPO 12:\n\n1. Camargo Soleto Gustavo Sebastián\n2. Saavedra Gómez Henry\n3. Villca Quiroga Ronald Nelson\n4. Serrano Soliz Abinadi"));
 	}
 
 	@Test
